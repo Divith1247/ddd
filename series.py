45a3931e06449f0fd34aaae5aff543464a649e1b
divith1247 <ashwinpm1971@gmail.com>
2025-11-15 21:43:46 +0530

    new prgm

--- series.py
+++ series.py
@@ -1,1 +1,17 @@
-print("this is divith:(")
+import pandas as pd
+from functools import reduce
+
+data = {
+    'num': [1,2,3,4,5],
+    'let': ['a','b','c','d','e']
+}
+
+df = pd.DataFrame(data)
+
+# square of numbers
+sq = list(map(lambda x: x**2, df['num']))
+print(sq)
+
+# even numbers
+ev = list(filter(lambda x: x % 2 == 0, df['num']))
+print(ev)
